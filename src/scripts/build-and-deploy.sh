0b4f0931fd5c0695e817363023ab6a118228f9a7
Bill Schumacher <bill@servernet.co>
2022-06-03 22:24:41 -0500

    add support for a tag prefix.

--- src/scripts/build-and-deploy.sh
+++ src/scripts/build-and-deploy.sh
@@ -5,7 +5,7 @@ if cat ~/.docker/config.json | grep "${ACCOUNT_URL}" > /dev/null 2>&1 ; then
 else
     aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" "$@" | docker login --username AWS --password-stdin "${ACCOUNT_URL}"
 fi
-IMAGE_AND_TAG=${AWS_RESOURCE_NAME_PREFIX}:${CIRCLE_SHA1}
+IMAGE_AND_TAG=${AWS_RESOURCE_NAME_PREFIX}:${PARAM_TAG_PREFIX}${CIRCLE_SHA1}
 docker build -f $PARAM_DOCKERFILE . -t $IMAGE_AND_TAG
 docker tag $IMAGE_AND_TAG $ACCOUNT_URL/$IMAGE_AND_TAG
 docker push $ACCOUNT_URL/$IMAGE_AND_TAG
