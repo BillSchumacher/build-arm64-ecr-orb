93b39c453bcdd32b61aff2724778a3474445e1c2
Bill Schumacher <34168009+BillSchumacher@users.noreply.github.com>
2022-11-08 03:20:14 -0600

    Add reading build args from env file.

--- src/scripts/build-and-deploy.sh
+++ src/scripts/build-and-deploy.sh
@@ -6,6 +6,12 @@ else
     aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" "$@" | docker login --username AWS --password-stdin "${ACCOUNT_URL}"
 fi
 IMAGE_AND_TAG=${AWS_RESOURCE_NAME_PREFIX}:${PARAM_TAG_PREFIX}${CIRCLE_SHA1}
-docker build -f $PARAM_DOCKERFILE . -t $IMAGE_AND_TAG
+
+if [[ -z "${PARAM_ENVFILE}" ]]; then
+  docker build -f $PARAM_DOCKERFILE . -t $IMAGE_AND_TAG
+else
+  docker build  $(cat $PARAM_ENVFILE | while read line; do out+="--build-arg $line"; done; echo $out; out="") .
+fi
+
 docker tag $IMAGE_AND_TAG $ACCOUNT_URL/$IMAGE_AND_TAG
 docker push $ACCOUNT_URL/$IMAGE_AND_TAG
