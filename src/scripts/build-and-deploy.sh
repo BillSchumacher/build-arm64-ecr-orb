df39823a8bda66fcb390aa3ae7703e8ad835afb3
Bill Schumacher <34168009+BillSchumacher@users.noreply.github.com>
2022-11-08 03:44:54 -0600

    Fix missing tag

--- src/scripts/build-and-deploy.sh
+++ src/scripts/build-and-deploy.sh
@@ -10,7 +10,7 @@ IMAGE_AND_TAG=${AWS_RESOURCE_NAME_PREFIX}:${PARAM_TAG_PREFIX}${CIRCLE_SHA1}
 if [[ -z "${PARAM_ENVFILE}" ]]; then
   docker build -f $PARAM_DOCKERFILE . -t $IMAGE_AND_TAG
 else
-  docker build  $(cat $PARAM_ENVFILE | while read line; do out+="--build-arg $line"; done; echo $out; out="") .
+  docker build  $(cat $PARAM_ENVFILE | while read line; do out+="--build-arg $line"; done; echo $out; out="") . -t $IMAGE_AND_TAG
 fi
 
 docker tag $IMAGE_AND_TAG $ACCOUNT_URL/$IMAGE_AND_TAG
